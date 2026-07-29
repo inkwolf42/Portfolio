01680182fbb1cf31614d5e251271e2e91134bd8b
InkWolf <inkwolf42@gmail.com>
2026-07-29 23:52:15 +0100

    Initialize React and some dependencies

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export default function App() {
+
+  return <div className='h-full bg-gray-100 flex justify-center items-center'>
+    <h1 className='text-9xl text-gray-300 font-bold'>Porfolio</h1>
+  </div>
+}
+
+
